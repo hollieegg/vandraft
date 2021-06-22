ecd726eb445330c9eb1b3fe2a391bb7962794e5f
RaghadEstanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-06-22 15:35:55 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Project2115
+# APST 62
+## Group: Vanritto
+
+Git Repository for final presentation of the Liberty Project Event Platform. 
+
+Team Members are: 
+
+Hollie Eggleton, Dana Mckee, Jennifer Debono, Raghad Estanbouli
+    
---
+++ vanritto.js
@@ -0,0 +1,44 @@
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//    document.getElementById("myDropdown").classList.toggle("show");
+//  }
+
+// // Close the dropdown if the user clicks outside of it
+//  window.onclick = function(event) {
+//    if (!event.target.matches('.dropbtn')) {
+//      var dropdowns = document.getElementsByClassName("dropdown-content");
+//      var i;
+//      for (i = 0; i < dropdowns.length; i++) {
+//        var openDropdown = dropdowns[i];
+//        if (openDropdown.classList.contains('show')) {
+//          openDropdown.classList.remove('show');
+//        }
+//      }
+//    }
+//  }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav" ) {
+    x.className += " responsive";
+    
+    if (document.getElementById("headertitle")!= null){
+      document.getElementById("headertitle").style.display="none"
+    }
+  } else {
+    x.className = "topnav";
+    if (document.getElementById("headertitle")!= null){
+    document.getElementById("headertitle").style.display="block"}
+
+  }
+}
+//https://www.w3schools.com/js/js_events.asp
+// code for button on Contact Us page submit popup
+function myFunctionpop() {
+  alert("Thank you");
+}
+
+
+
